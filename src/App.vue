b82f04600dee44a94d18fecc2e18d8de550bdd44
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-06-07 01:16:34 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <SplashView />
+  </div>
+</template>
+
+<script>
+import SplashView from "./components/SplashView.vue";
+
+export default {
+  name: "App",
+  components: {
+    SplashView,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
