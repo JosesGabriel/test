15787d9d8e4d1db78466c662ee4053fc7e0a2385
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-06-08 07:50:29 +0800

    Edited description about me

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="flex description__section">
-    <img class="me" src="../assets/ses.png" />
-    <span class="description__heading">Hello, I'm Joses.</span>
+    <img class="me" src="../assets/images/ses2.png" />
+    <span class="description__heading"
+      >Hello, I'm <span class="secondary--text">Joses.</span></span
+    >
     <div class="description__body">
-      I'm a developer with an affinity towards Front End Development but am
-      confident in my abilities in both facets.
+      I'm a developer with an affinity towards Front End Development but can
+      switch to Back End when the need arises.
     </div>
     <br />
     <div class="description__body">
@@ -26,6 +28,7 @@ section {
 .description__section {
   padding-top: 40px;
   font-size: 2em;
+  margin-bottom: 80px;
 }
 .description__heading {
   font-weight: bold;
