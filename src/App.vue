4e23c12d8baf45462eff9d2e2cad5ac4920f77f6
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-06-07 15:31:34 +0800

    Added About Me section

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,45 @@
 <template>
   <div id="app">
     <SplashView />
+    <About />
   </div>
 </template>
 
 <script>
 import SplashView from "./components/SplashView.vue";
+import About from "./components/About.vue";
 
 export default {
   name: "App",
   components: {
     SplashView,
+    About,
   },
 };
 </script>
 
 <style>
+html {
+  max-width: 100%;
+  overflow-x: hidden;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-size: 10px;
 }
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
 body {
   margin: 0;
-  /* background-color: purple; */
 }
 .test {
-  height: 1000px;
+  height: 70vh;
 }
 .flex {
   display: flex;
---
+++ src/components/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="flex description__section">
+    <img class="me" src="../assets/ses.png" />
+    <span class="description__heading">Hello, I'm Joses.</span>
+    <div class="description__body">
+      I'm a developer with an affinity towards Front End Development but am
+      confident in my abilities in both facets.
+    </div>
+    <br />
+    <div class="description__body">
+      I have participated in Programming competitions (once outside the
+      country!) and eventually grew to judge a few. I've spoken at technical
+      conferences and loved it so much that I also teach programming at
+      <a href="https://mcm.edu.ph/" target="#">Malayan College Mindanao</a>.
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.flex {
+  justify-content: unset;
+}
+section {
+  height: 80vh;
+}
+.description__section {
+  padding-top: 40px;
+  font-size: 2em;
+}
+.description__heading {
+  font-weight: bold;
+  font-size: 2.1em;
+}
+.description__body {
+  width: 35vw;
+  text-align: center;
+}
+.me {
+  width: 200px;
+  padding-bottom: 20px;
+}
+</style>
