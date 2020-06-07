7f1cb2fd50a9fde10198159034581e2a510e7c06
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-06-07 12:11:22 +0800

    Added WebGL splash screen

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,19 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 10px;
+}
+body {
+  margin: 0;
+  /* background-color: purple; */
+}
+.test {
+  height: 1000px;
+}
+.flex {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
--- src/components/SplashView.vue
+++ src/components/SplashView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="container"></div>
+  <section class="flex">
+    <div class="canvas" ref="container"></div>
+    <div class="flex text canvas__text">
+      <div>Hello, I'm Joses.</div>
+      Software Engineer & Educator.
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,47 +17,95 @@ export default {
       scene: null,
       renderer: null,
       mesh: null,
+      stars: null,
+      starsGeo: null,
+      starImg: require("../assets/star.png"),
     };
   },
   mounted() {
-    this.init();
-    this.animate();
+    this.initialize();
   },
   methods: {
-    init: function () {
+    initialize() {
+      let container = this.$refs["container"];
       this.scene = new THREE.Scene();
+      this.scene.background = new THREE.Color(0x0c1821);
       this.camera = new THREE.PerspectiveCamera(
-        75,
+        60,
         window.innerWidth / window.innerHeight,
-        0.1,
+        1,
         1000
       );
+      this.camera.position.z = 1;
+      this.camera.rotation.x = Math.PI / 2;
 
       this.renderer = new THREE.WebGLRenderer();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(this.renderer.domElement);
+      container.append(this.renderer.domElement);
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      this.cube = new THREE.Mesh(geometry, material);
-      this.scene.add(this.cube);
+      this.starGeo = new THREE.Geometry();
+      for (let i = 0; i < 3000; i++) {
+        let star = new THREE.Vector3(
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300
+        );
 
-      this.camera.position.z = 5;
+        star.velocity = 0;
+        star.acceleration = 0.002;
+        this.starGeo.vertices.push(star);
+      }
+
+      let sprite = new THREE.TextureLoader().load(this.starImg);
+      let starMaterial = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 0.7,
+        map: sprite,
+      });
+
+      this.stars = new THREE.Points(this.starGeo, starMaterial);
+      this.scene.add(this.stars);
+
+      window.addEventListener("resize", this.onWindowResize, false);
+
+      this.animate();
+    },
+    onWindowResize() {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
     },
     animate: function () {
-      requestAnimationFrame(this.animate);
-
-      this.cube.rotation.x += 0.01;
-      this.cube.rotation.y += 0.01;
-
+      this.starGeo.vertices.forEach((p) => {
+        p.velocity += p.acceleration;
+        p.x -= p.velocity;
+        if (p.x < -300) {
+          p.x = window.innerWidth / 5;
+          p.velocity = 0;
+        }
+      });
+      this.starGeo.verticesNeedUpdate = true;
       this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(this.animate);
     },
   },
 };
 </script>
 
 <style scoped>
-div {
-  background-color: #add8e6;
+.text {
+  color: white;
+  font-size: 1.5rem;
+}
+.canvas {
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  z-index: -1;
+}
+.canvas__text {
+  height: 100vh;
 }
 </style>
